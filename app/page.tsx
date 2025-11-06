c9d7f764f58f3bd0dcb66caeb3690ad871637db0
Aaryan <hi.aaryankapoor@gmail.com>
2025-11-07 02:24:45 +0530

    initial ⚡️

--- app/page.tsx
+++ app/page.tsx
@@ -2,62 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col justify-between p-8 lg:py-24 items-start">
+        <div className="h-8" />
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="max-w-x items-center flex text-2xl gap-2 font-medium leading-0 tracking-tight text-white">
+            <Image
+              className="-ml-2"
+              src="/logo.png"
+              alt="Next.js logo"
+              width={36}
+              height={36}
+              priority
+            />{" "}
+            Wora
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-md text-sm text-white opacity-70">
+            An elegant lossless music player for your machine.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div className="flex gap-4 text-base font-medium items-center flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex h-8 w-fit items-center justify-center text-xs gap-2 rounded-2xl px-5 bg-white text-black  dark:hover:bg-[#ccc]"
+            href="https://github.com/playwora/wora/releases/tag/v0.4.0-beta1"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/logo.png"
+              className="invert"
+              alt="wora logo"
               width={16}
               height={16}
             />
-            Deploy Now
+            Download
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <p className="max-w-md text-xs text-white opacity-70 font-mono">
+            v0.4.0 [Beta 1]
+          </p>
         </div>
       </main>
     </div>
